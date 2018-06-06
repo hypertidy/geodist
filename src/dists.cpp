23de468373bab17164409a3334be3c5a37d3d720
mpadge <mark.padgham@email.com>
2018-06-06 12:18:02 +0200

    already found an efficiency boost

--- src/dists.cpp
+++ src/dists.cpp
@@ -10,8 +10,10 @@ double rcpp_haversine (double x1, double y1, double x2, double y2)
 {
     double xd = (x2 - x1) * M_PI / 180.0;
     double yd = (y2 - y1) * M_PI / 180.0;
-    double d = sin (yd / 2.0) * sin (yd / 2.0) + cos (y2 * M_PI / 180.0) *
-        cos (y1 * M_PI / 180.0) * sin (xd / 2.0) * sin (xd / 2.0);
+    double sxd = sin (xd / 2.0);
+    double syd = sin (yd / 2.0);
+    double d = syd * syd + cos (y2 * M_PI / 180.0) *
+        cos (y1 * M_PI / 180.0) * sxd * sxd;
     d = 2.0 * earth * asin (sqrt (d));
     return (d);
 }

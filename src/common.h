13b1917f532544406954b3146810f1f17c0cf936
mpadge <mark.padgham@email.com>
2018-06-14 10:30:24 +0200

    src file restructure

--- src/common.h
+++ src/common.h
@@ -1,9 +1,10 @@
-const double earth = 6378.137; // value used in geosphere::distHaversine
-const double meridian = 12429.9; // length of prime meridian in metres
-const double equator = 40007.862917; // equatorial circumference
+#ifndef COMMON_H
+#define COMMON_H
 
 double one_haversine (double x1, double y1, double x2, double y2,
         double cosy1, double cosy2);
 double one_vincenty (double x1, double y1, double x2, double y2,
         double siny1, double cosy1, double siny2, double cosy2);
 double one_cheap (double x1, double y1, double x2, double y2, double cosy);
+
+#endif /* COMMON_H */
---
+++ src/dists_xy.c
@@ -0,0 +1,120 @@
+#include <R.h>
+#include <Rinternals.h>
+
+#include "common.h"
+
+//' R_haversine_xy
+//' @param x_ Single vector of x-values in [1:n], y-values in [n+(1:n)]
+//' @param y_ Additional vector of x-values in [1:n], y-values in [n+(1:n)]
+//' @noRd
+SEXP R_haversine_xy (SEXP x_, SEXP y_)
+{
+    size_t nx = floor (length (x_) / 2);
+    size_t ny = floor (length (y_) / 2);
+    size_t n2 = nx * ny;
+    //Rprintf ("(nx, ny) = (%d , %d )\n", nx, ny);
+    SEXP out = PROTECT (allocVector (REALSXP, n2));
+    double *rx, *ry, *rout;
+    rx = REAL (x_);
+    ry = REAL (y_);
+    rout = REAL (out);
+
+    for (size_t i = 0; i < nx; i++)
+    {
+        double cosy1 = cos (rx [nx + i] * M_PI / 180.0); // y-value of x data
+        for (size_t j = 0; j < ny; j++)
+        {
+            double cosy2 = cos (ry [ny + j] * M_PI / 180.0);
+            rout [i * ny + j] = one_haversine (rx [i], rx [nx + i],
+                    ry [j], ry [ny + j], cosy1, cosy2);
+        }
+    }
+
+    UNPROTECT (1);
+
+    return out;
+}
+
+//' R_vincenty_xy
+//' @param x_ Single vector of x-values in [1:n], y-values in [n+(1:n)]
+//' @param y_ Additional vector of x-values in [1:n], y-values in [n+(1:n)]
+//' @noRd
+SEXP R_vincenty_xy (SEXP x_, SEXP y_)
+{
+    size_t nx = floor (length (x_) / 2);
+    size_t ny = floor (length (y_) / 2);
+    size_t n2 = nx * ny;
+    //Rprintf ("(nx, ny) = (%d , %d )\n", nx, ny);
+    SEXP out = PROTECT (allocVector (REALSXP, n2));
+    double *rx, *ry, *rout;
+    rx = REAL (x_);
+    ry = REAL (y_);
+    rout = REAL (out);
+
+    for (size_t i = 0; i < nx; i++)
+    {
+        double siny1 = sin (rx [nx + i] * M_PI / 180.0); // y-value of x data
+        double cosy1 = cos (rx [nx + i] * M_PI / 180.0); // y-value of x data
+        for (size_t j = 0; j < ny; j++)
+        {
+            double siny2 = sin (ry [ny + j] * M_PI / 180.0);
+            double cosy2 = cos (ry [ny + j] * M_PI / 180.0);
+            rout [i * ny + j] = one_vincenty (rx [i], rx [nx + i],
+                    ry [j], ry [ny + j], siny1, cosy1, siny2, cosy2);
+        }
+    }
+
+    UNPROTECT (1);
+
+    return out;
+}
+
+//' R_cheap_xy
+//' @param x_ Single vector of x-values in [1:n], y-values in [n+(1:n)]
+//' @noRd
+SEXP R_cheap_xy (SEXP x_, SEXP y_)
+{
+    size_t nx = floor (length (x_) / 2);
+    size_t ny = floor (length (y_) / 2);
+    size_t n2 = nx * ny;
+    SEXP out = PROTECT (allocVector (REALSXP, n2));
+    double *rx, *ry, *rout;
+    rx = REAL (x_);
+    ry = REAL (y_);
+    rout = REAL (out);
+
+    // Get maximal latitude range
+    double ymin = 9999.9, ymax = -9999.9;
+    for (size_t i = 0; i < nx; i++)
+    {
+        if (rx [nx + i] < ymin)
+            ymin = rx [nx + i];
+        else if (rx [nx + i] > ymax)
+            ymax = rx [nx + i];
+    }
+    for (size_t i = 0; i < ny; i++)
+    {
+        if (ry [ny + i] < ymin)
+            ymin = ry [ny + i];
+        else if (ry [ny + i] > ymax)
+            ymax = ry [ny + i];
+    }
+    // and set constant cosine multiplier
+    ymin = ymin * M_PI / 180;
+    ymax = ymax * M_PI / 180;
+    double cosy = cos (ymin + ymax) / 2.0;
+
+    for (size_t i = 0; i < nx; i++)
+    {
+        for (size_t j = 0; j < ny; j++)
+        {
+            rout [i * ny + j] = one_cheap (rx [i], rx [nx + i],
+                    ry [j], ry [ny + j], cosy);
+        }
+    }
+
+    UNPROTECT (1);
+
+    return out;
+}
+
